553e264a202683274491b376a3174c596c0ae4be
pompyn <pompyn@live.com>
2021-08-17 15:32:07 -0500

    add models

---
+++ box/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField(null=False, blank=False)
+    time_required = models.CharField(max_length=30)
+    instructions = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
