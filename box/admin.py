bce89681d2abfc5ce0661455f39f3a16a8ac0517
pompyn <pompyn@live.com>
2021-08-17 15:35:51 -0500

    add superuser and register models

--- box/admin.py
+++ box/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from box.models import Author, Recipe
+# Register your models here.
+admin.site.register(Author)
+admin.site.register(Recipe)
