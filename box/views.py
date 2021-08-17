a9cbc8023c982aded41d47cd642bf92cb6b80010
pompyn <pompyn@live.com>
2021-08-17 16:05:35 -0500

    add url, edit settings, add templates

--- box/views.py
+++ box/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from box.models import Recipe, Author
 
 # Create your views here.
+
+
+def index_view(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'index.html', {'recipes': recipes})
+
+
+def recipe_detail(request, id):
+    recipe = Recipe.objects.get(id=id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
+
+
+def author_detail(request, id):
+    author = Author.objects.get(id=id)
+    recipes = Recipe.objects.filter(author=author)
+    return render(request, 'author_detail.html', {'author': author, 'recipes': recipes})
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from box import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('author/<int:id>/', views.author_detail, name='author'),
+    path('recipe/<int:id>/', views.recipe_detail, name='recipe'),
+    path('', views.index_view, name='homepage'),
+
 ]
